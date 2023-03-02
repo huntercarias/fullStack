bb56a9dab3c3dbf73fb0cca6f00b3d21180864f6
Hunter Carias <huntercarias@hotmail.com>
2023-03-02 06:57:01 -0600

    formulario

--- frondEnd/pagina-2023/src/components/pika.jsx
+++ frondEnd/pagina-2023/src/components/pika.jsx
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from 'react'
+import axios from "axios";
+import React from "react";
+
+const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
 
 
 const pika = () => {
@@ -26,6 +30,28 @@ const pika = () => {
                     </input>
                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
+
+                <div class="mb-3">
+                    <label for="disabledTextInput" class="form-label">Telefono</label>
+                    <input type="text" id="Apellidos" class="form-control" placeholder="Disabled input">
+                    </input>
+                </div>
+
+
+                <div class="mb-3">
+                    <label for="disabledTextInput" class="form-label">Fecha Nacimiento</label>
+                    <input type="text" id="Apellidos" class="form-control" placeholder="Disabled input">
+                    </input>
+                </div>
+
+
+                <div class="mb-3">
+                    <label for="disabledTextInput" class="form-label">Mensaje</label>
+                    <input type="text" id="Apellidos" class="form-control" placeholder="Disabled input">
+                    </input>
+                </div>
+
+
 
             </div>
         </form>
